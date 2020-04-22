e7fe9e59cd184e9fa0f725c14bfae503f60c1b25
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-04-23 00:44:37 +0700

    update store

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,87 +1,98 @@
-const Store = require('../models/Store')
+const Store = require("../models/Store");
 
-const index = async(req, res) => {
-  try{
+const index = async (req, res) => {
+  try {
     const stores = await Store.find();
     return res.status(200).json({
-      success:true,
-      count:stores.length,
-      data:stores
-    })
-  }catch(err){
-    console.log(err)
+      success: true,
+      count: stores.length,
+      data: stores,
+    });
+  } catch (err) {
+    console.log(err);
   }
-}
-const store = async(req, res) => {
-  try{
+};
+const store = async (req, res) => {
+  try {
     const data = {
-      storeId : req.body.storeId,
-      address : req.body.address,
-      location:{
-        type:req.body.location.type,
-        coordinates:req.body.location.coordinates,
-        formattedAddress: req.body.location.formattedAddress
-      }
-    }
+      storeId: req.body.storeId,
+      address: req.body.address,
+    };
     const store = await Store.create(data);
-    return res.status(201).json({
-      success:true,
-      data:store
-    })
-  }catch(err){
-    console.log(err)
-  }
-}
-const show = async(req, res) => {
-  try{
-    const stores = await Store.findOne({_id:req.params.id});
-    return res.status(200).json({
-      success:true,
-      count:stores.length,
-      data:stores
-    })
-  }catch(err){
-    console.log(err)
-  }
-}
-const update = async(req, res) => {
-  try{
-    const data = {
-      storeId : req.body.storeId,
-      address : req.body.address,
-      location:{
-        type:req.body.location.type,
-        coordinates:req.body.location.coordinates,
-        formattedAddress: req.body.location.formattedAddress
-      }
+    if (store) {
+      return res.status(201).json({
+        success: true,
+        data: store,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+      });
     }
-    const store = await Store.updateOne({_id:req.params.id},{$set:{data}});
-    return res.status(200).json({
-      success:true,
-      count:store.length,
-      data:store
-    })
-  }catch(err){
-    console.log(err)
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message:err.message
+    });
   }
-}
-const destroy = async(req, res) => {
-  try{
-    const store = await Store.deleteOne({_id:req.params.id});
+};
+const show = async (req, res) => {
+  try {
+    const stores = await Store.findOne({ _id: req.params.id });
     return res.status(200).json({
-      success:true,
-      count:stores.length,
-      data:store
-    })
-  }catch(err){
-    console.log(err)
+      success: true,
+      count: stores.length,
+      data: stores,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message:err.message
+    });
   }
-}
+};
+const update = async (req, res) => {
+  try {
+    const data = {
+      storeId: req.body.storeId,
+      address: req.body.address,
+    };
+    const store = await Store.updateOne(
+      { _id: req.params.id },
+      { $set: { data } }
+    );
+    return res.status(200).json({
+      success: true,
+      count: store.length,
+      data: store,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message:err.message
+    });
+  }
+};
+const destroy = async (req, res) => {
+  try {
+    const store = await Store.deleteOne({ _id: req.params.id });
+    return res.status(200).json({
+      success: true,
+      count: stores.length,
+      data: store,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message:err.message
+    });
+  }
+};
 
 module.exports = {
   index,
   store,
   show,
   update,
-  destroy
-}
+  destroy,
+};
